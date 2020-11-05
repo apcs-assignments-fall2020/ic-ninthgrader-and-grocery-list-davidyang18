1e4977eeb74b7de91c3074fe51939f08d49d891f
davidyang18 <2817822828@qq.com>
2020-11-05 06:19:05 +0000

    Grocery List

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,31 @@
+import java.util.Arrays;
+
 public class GroceryList {
-    
+    private String [] arr;
+
+    public GroceryList() {
+        this.arr = new String[5];
+    }
+
+    public void add(String item) {
+        for (int i = 0; i < 4; i++) {
+            if (arr[i] == null) {
+                arr[i] = item;
+            }
+        }
+    }
+    public void remove(String item) {
+        for (int i = 0; i < 4; i++) {
+            if (arr[i] == item) {
+                arr[i] = null;
+                for (int j = 0; j < 4; j++) {
+                    arr[j] = arr[j-1];
+                }
+            }
+        }
+    }
+    public String toString() {
+        String str = Arrays.toString(arr);
+        return str;
+    }
 }
--- src/main/java/NinthGrader.java
+++ src/main/java/NinthGrader.java
@@ -1,3 +1,15 @@
-public class NinthGrader {
-    
+public class NinthGrader extends Student {
+    public NinthGrader (String firstName, String lastName) {
+        super(firstName, lastName, 2024);
+    }
+    public NinthGrader() {
+        super("James", "D", 2024); 
+    }
+    public void doSomething() {
+        System.out.println("I'm preparing for a test.");
+    }
+    public String toString() {
+        String str = this.getFirstName() + " " + this.getLastName() + "," + this.getClassYear() + "Ninth Grader";
+        return str;
+    }
 }
